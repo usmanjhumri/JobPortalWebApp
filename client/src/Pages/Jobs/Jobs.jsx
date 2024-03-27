781b1d778a4603a898c42878951c95024f9d44ee
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-27 14:52:07 +0500

    working on client page

--- client/src/Pages/Jobs/Jobs.jsx
+++ client/src/Pages/Jobs/Jobs.jsx
@@ -2,13 +2,28 @@ import { Box, Container, Grid, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import styles from "../../components/SignUp/SignUpStyle";
+import TextImg from '../../assets/hometextBG.png'
 
 const Jobs = () => {
     return (
         <>
+
             <Box sx={styles.SingUp} className="sinupSingin">
                 <Container>
                     <Grid container>
+                        <Box>
+                            <span className='circle'></span>
+                            <span className='circle circle-yellow'></span>
+                            <span className='shape-plus'>+</span>
+                        </Box>
+                        <Box sx={{
+                            position: "absolute",
+                            left: "16%",
+                            top: "30%",
+                            display: { md: "block", xs: "none" }
+                        }}>
+                            <Box component="img" maxWidth="100%" src={TextImg} />
+                        </Box>
                         <Box>
                             <Box sx={styles.containerBox}>
                                 <Typography
@@ -24,6 +39,13 @@ const Jobs = () => {
                                 </Box>
                             </Box>
                         </Box>
+                    </Grid>
+                </Container>
+            </Box>
+            <Box>
+                <Container>
+                    <Grid container>
+
                     </Grid>
                 </Container>
             </Box>
