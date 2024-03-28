2b7c65cb62e49fd8afc28f72cfc4dfa9fdaefbf5
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-28 12:13:08 +0500

    working on jobs page

--- client/src/Pages/Jobs/Jobs.jsx
+++ client/src/Pages/Jobs/Jobs.jsx
@@ -9,8 +9,11 @@ import LocationOn from "@mui/icons-material/LocationOn";
 import jobsStyle from "./JobsStyle";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { RiCalendar2Line } from "react-icons/ri";
-import DS from '../../assets/jobsIMGDataScience.jpg'
 import { VscLocation } from "react-icons/vsc";
+import UIUX from '../../assets/uiuxDesigner.jpeg'
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { MdHistory } from "react-icons/md";
+import { BsArrowRightShort } from "react-icons/bs";
 
 const Jobs = () => {
     return (
@@ -52,19 +55,11 @@ const Jobs = () => {
             </Box>
             <Box mt={6}>
                 <Container>
-                    <Box sx={{
-                        display: { md: "flex", xs: "wrap" },
-                        alignItems: "center",
-                        gap: 3,
-                    }}>
+                    <Box sx={jobsStyle.jobSearchmainBox}>
                         <Input fullWidth
                             placeholder="Your location"
                             endDecorator={<LocationOn sx={{ color: "#fe9703", fontSize: "18px" }} />}
-                            sx={{
-                                padding: "0.7rem 1rem",
-                                margin: "0.7rem 0"
-
-                            }}
+                            sx={jobsStyle.jobSearchInput}
                         />
                         <Select autoFocus
                             placeholder="Select a Job..."
@@ -90,39 +85,42 @@ const Jobs = () => {
                                         PART TIME
                                     </Typography>
                                     <Box sx={jobsStyle.jobandcalendar}>
-                                        <RiCalendar2Line style={{ fontSize: "16px", color: "#26ae61" }} /> <Typography sx={jobsStyle.jobpostedDate}>
+                                        <RiCalendar2Line style={{ fontSize: "16px", color: "#26ae61" }} />
+                                        <Typography sx={jobsStyle.jobpostedDate}>
                                             2 MONTHS AGO
                                         </Typography>
                                     </Box>
                                 </Box>
-                                <Box mt={5}>
+                                <Box mt={5} sx={jobsStyle.commonStyle}>
                                     <Box>
-                                        <img src={DS} alt="job picutre" width={80} height={80} style={{ borderRadius: "50%" }} />
+                                        <img src={UIUX} alt="job picutre" width={70} height={70} style={{ borderRadius: "50%" }} />
                                     </Box>
                                     <Box>
-                                        <Typography variant="h4" sx={{
-                                            fontSize: "18px",
-                                            fontWeight: 700,
-                                        }}>
+                                        <Typography variant="h4" sx={jobsStyle.jobname}>
                                             UI/UX Designer
                                         </Typography>
-                                        <a href="http://google.com" style={{
-                                            textDecoration: "none",
-                                            color: "#26ae61",
-                                            margin: "2px 0",
-                                            textTransform: "capitalize",
-                                            fontSize: "14px"
-                                        }}>Google</a>
-                                        <Box sx={{
-                                            display: "flex",
-                                            alignItems: "center"
-                                        }}>
-                                            <VscLocation /> <Typography sx={{
-                                                color: "#667488",
-                                                fontSize: "12px"
-                                            }}>Florida</Typography>
+                                        <a href="http://google.com" style={jobsStyle.companyname}>Google</a>
+                                        <Box sx={jobsStyle.companyloaction}>
+                                            <VscLocation style={{ fontSize: "20px", color: "#26ae61" }} /> <Typography sx={jobsStyle.companycountry}>Florida</Typography>
                                         </Box>
                                     </Box>
+                                </Box>
+                                <Box mt={5} sx={jobsStyle.commonStyle}>
+                                    <Typography sx={{ fontSize: "13px" }}>$ 1.2k - 3k</Typography>
+                                    <Typography sx={jobsStyle.experienceandpay}><IoBriefcaseOutline style={{ color: "#26ae61", fontSize: "16px" }} />Experience : 2 Years</Typography>
+                                </Box>
+
+                                <Box mt={5} sx={jobsStyle.lastdateAndJobDetail}>
+                                    <Typography sx={jobsStyle.jobLastDate}>
+                                        <MdHistory style={{ fontSize: "18px", color: "#26ae61" }} />15 Days Left To Apply
+                                    </Typography>
+                                    <Typography sx={jobsStyle.jobDetailAndIcon}>
+                                        <Link to="/" style={jobsStyle.jobDetailLink}>
+                                            Job Details
+                                        </Link>
+                                        <BsArrowRightShort style={{ fontSize: "20px", color: "#26ae61" }} />
+                                    </Typography>
+
                                 </Box>
                             </Box>
                         </Grid>
--- client/src/Pages/Jobs/JobsStyle.jsx
+++ client/src/Pages/Jobs/JobsStyle.jsx
@@ -31,5 +31,72 @@ const jobsStyle = {
         padding: "0 10px",
 
     },
+    companyname: {
+        textDecoration: "none",
+        color: "#26ae61",
+        margin: "3px 0",
+        textTransform: "capitalize",
+        fontSize: "14px"
+    },
+    companyloaction: {
+        display: "flex",
+        alignItems: "center",
+        margin: "3px 0",
+        marginLeft: "-6px"
+    },
+    companycountry: {
+        color: "#667488",
+        fontSize: "12px",
+        margin: "3px 0"
+
+    },
+    jobname: {
+        fontSize: "18px",
+        fontWeight: 700,
+    },
+    commonStyle: {
+        display: "flex",
+        alignItems: "center",
+        gap: 3
+    },
+    lastdateAndJobDetail: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    experienceandpay: {
+        display: "flex",
+        alignItems: "center",
+        gap: 1,
+        fontSize: "13px"
+    },
+    jobSearchmainBox: {
+        display: { md: "flex", xs: "wrap" },
+        alignItems: "center",
+        gap: 3,
+    },
+    jobSearchInput: {
+        padding: "0.7rem 1rem",
+        margin: "0.7rem 0"
+    },
+    jobLastDate: {
+        fontSize: "13px",
+        display: "flex",
+        alignItems: "center",
+        gap: 0.2,
+        marginLeft: "-6px"
+    },
+    jobDetailAndIcon: {
+        display: "flex",
+        alignItems: "center",
+        gap: 0.4,
+    },
+    jobDetailLink: {
+        fontSize: "14px",
+        fontWeight: 700,
+        color: "#26ae61",
+        textDecoration: "none"
+    },
+
 }
 export default jobsStyle
