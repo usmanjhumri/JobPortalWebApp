762de97929b09e9bf5d94c02d4c4acb4be805988
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-29 06:31:58 +0500

    working on job detail page

--- client/src/Pages/JobDetail/JobDetail.jsx
+++ client/src/Pages/JobDetail/JobDetail.jsx
@@ -1,9 +1,21 @@
+import { Box, Container, Grid } from "@mui/material"
 import CommonPage from "../../components/commonPage/CommonPage"
 
 const JobDetail = () => {
     return (
         <>
             <CommonPage value="Job Detail" />
+            <Box>
+                <Container sx={{
+                    borderTop: "5px solid #fe9703",
+                    background: "#FFF",
+                    padding: "50px"
+                }}>
+                    <Grid container>
+
+                    </Grid>
+                </Container>
+            </Box>
 
         </>
     )
