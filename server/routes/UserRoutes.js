1f9dc93445bc20e023f2b1895f5892694b71bd2e
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-29 11:46:38 +0500

    working on application form API

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -3,9 +3,11 @@ import UserController from "../controllers/userController.js";
 import checkUserAuth from "../middlerwares/auth-middleware.js";
 const router = express.Router();
 
+// Private Route
 router.use("/userlogged", checkUserAuth);
 router.get("/userlogged", UserController.userLoggedData);
 
+// Public Route
 router.post("/register", UserController.userRigerstration);
 router.post("/login", UserController.userLoggedIn);
 router.post("/mail-sent", UserController.forgotPassword);
