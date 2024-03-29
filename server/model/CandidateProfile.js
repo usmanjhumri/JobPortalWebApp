13c2a7f6bfade8639b692af825454353a2bc081d
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-29 12:40:41 +0500

    working on applied job API

---
+++ server/model/CandidateProfile.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const CandidateSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true },
+  dob: { type: Date, required: true },
+  address: { type: String, required: true, trim: true },
+  portfolio: { type: String, required: true, trim: true },
+  rdoc: { type: String, required: true },
+  phone: { type: String, required: true, trim: true },
+  salary: { type: String, required: true, trim: true },
+  hearabout: { type: String, trim: true },
+  coverletter: { type: String, trim: true },
+});
+
+const UserApplication = mongoose.model("candidateApplication", CandidateSchema);
+
+export default UserApplication;
