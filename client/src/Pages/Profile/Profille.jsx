1540826da3975de9ef393930c9e53fc4d6f115c6
usmanlatif123 <usman@enigmadevcraft.com>
2024-04-15 08:55:48 +0500

    working on profile

--- client/src/Pages/Profile/Profille.jsx
+++ client/src/Pages/Profile/Profille.jsx
@@ -1,20 +1,23 @@
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Box, Container, Typography } from '@mui/material';
 import CommonPage from '../../components/commonPage/CommonPage';
 import React from 'react';
+import { Input } from '@mui/joy';
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { AiOutlinePhone } from "react-icons/ai";
+import { Button } from '@mui/joy';
+import contactStyle from '../ContactUs/contactStyle';
+import { FaRegAddressCard } from "react-icons/fa";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-
   return (
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -32,32 +35,29 @@ TabPanel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-// function a11yProps(index) {
-//   return {
-//     id: `vertical-tab-${index}`,
-//     'aria-controls': `vertical-tabpanel-${index}`,
-//   };
-// }
+
 
 export default function Profille() {
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const handleNext = () => {
+    setValue((pre) => pre + 1)
+  }
   return (
     <>
       <CommonPage value="Profile" />
 
-      <Box mt={12}
-        sx={{ flexGrow: 1, display: 'flex', }}
-      >
+      <Box mt={12}>
+
         <Tabs
-          orientation="vertical"
           variant="scrollable"
           value={value}
           onChange={handleChange}
-          aria-label="Vertical tabs example"
-          sx={{ borderRight: 1, borderColor: 'divider' }}
+          scrollButtons
+          allowScrollButtonsMobile
+          TabIndicatorProps={{ sx: { display: "none" } }}
         >
           <Tab label="Profile" />
           <Tab label="Personal Information" />
@@ -65,21 +65,97 @@ export default function Profille() {
           <Tab label="Skills" />
           <Tab label="Experience" />
         </Tabs>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          Item Four
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          Item Five
-        </TabPanel>
+
+        <Container>
+          <TabPanel value={value} index={0}>
+            <Box component="form">
+              <Box sx={contactStyle.fullNameEmail}>
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter Your First Name"
+                  endDecorator={<FaRegUser style={contactStyle.inputicons} />}
+                />
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter Your Last Name"
+                  endDecorator={<FaRegUser style={contactStyle.inputicons} />}
+                />
+              </Box>
+              <Box sx={contactStyle.fullNameEmail}>
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter your Email Address"
+                  endDecorator={<MdOutlineEmail style={contactStyle.inputicons} />}
+                />
+                <Input fullWidth
+                  placeholder="Enter Your Phone Number"
+                  endDecorator={<AiOutlinePhone style={contactStyle.inputicons} />}
+                  sx={{ margin: "1rem 0" }}
+                />
+              </Box>
+              <Input fullWidth
+                placeholder="Enter Your Address"
+                endDecorator={<FaRegAddressCard style={contactStyle.inputicons} />}
+                sx={{ margin: "1rem 0" }}
+              />
+              <Box sx={contactStyle.fullNameEmail}>
+                <Button fullWidth sx={{ background: "#26ae61", margin: "1rem 0" }} type='submit' >
+                  Update
+                </Button>
+                <Button onClick={handleNext} fullWidth sx={{ background: "#26ae61", margin: "1rem 0" }}  >
+                  Next
+                </Button>
+              </Box>
+
+            </Box>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Box component="form">
+              <Box sx={contactStyle.fullNameEmail}>
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter Your First Name"
+                  endDecorator={<FaRegUser style={contactStyle.inputicons} />}
+                />
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter Your Last Name"
+                  endDecorator={<FaRegUser style={contactStyle.inputicons} />}
+                />
+              </Box>
+              <Box sx={contactStyle.fullNameEmail}>
+                <Input fullWidth sx={{ margin: "1rem 0" }}
+                  placeholder="Enter your Email Address"
+                  endDecorator={<MdOutlineEmail style={contactStyle.inputicons} />}
+                />
+                <Input fullWidth
+                  placeholder="Enter Your Phone Number"
+                  endDecorator={<AiOutlinePhone style={contactStyle.inputicons} />}
+                  sx={{ margin: "1rem 0" }}
+                />
+              </Box>
+              <Input fullWidth
+                placeholder="Enter Your Address"
+                endDecorator={<FaRegAddressCard style={contactStyle.inputicons} />}
+                sx={{ margin: "1rem 0" }}
+              />
+              <Box sx={contactStyle.fullNameEmail}>
+                <Button fullWidth sx={{ background: "#26ae61", margin: "1rem 0" }} type='submit' >
+                  Update
+                </Button>
+                <Button onClick={handleNext} fullWidth sx={{ background: "#26ae61", margin: "1rem 0" }}  >
+                  Next
+                </Button>
+              </Box>
+
+            </Box>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Item Three
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            Item Four
+          </TabPanel>
+          <TabPanel value={value} index={4}>
+            Item Five
+          </TabPanel>
+        </Container>
+
       </Box>
     </>
   );
