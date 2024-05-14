c340f06d49d2b3027de9ca7c4e83648c479e4b07
usmanlatif123 <usman@enigmadevcraft.com>
2024-05-14 12:03:10 +0500

    working on voice base search

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import { Container, Grid, Typography, Box } from "@mui/material";
 import HomeStyle from "./HomeStyle";
 import Popular from "../PopularCategory/PopularCategory";
 import NewsandBlogs from "../Latest News & Blog/News&Blog";
-
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
 import LocationOn from "@mui/icons-material/LocationOn";
@@ -20,43 +19,54 @@ import { GetCategory, GetJobs } from "../../RTK/API/api";
 import { GetJobDetails } from "../../RTK/Slice/JobSlice";
 import Loader from "../../components/Loader/Loader";
 import { GetCategoryStatus } from "../../RTK/Slice/CategoriesSlice";
-// import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import { useNavigate } from "react-router-dom";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { MdKeyboardVoice } from "react-icons/md";
 import { IoStopCircleOutline } from "react-icons/io5";
+
 const Home = () => {
   const dispatch = useDispatch();
   const { JobStatus, jobs } = useSelector(GetJobDetails);
   const { categoryStatus, Categories } = useSelector(GetCategoryStatus);
   const [values, setvalues] = useState({ title: "", location: "" });
   const [isListening, setIsListening] = useState(false);
+  const [manualEdit, setManualEdit] = useState(false); // New state to track manual edits
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!jobs || !Categories) {
       dispatch(GetJobs());
       dispatch(GetCategory());
     }
-  }, []);
+  }, [dispatch, jobs, Categories]);
+
+
   const handleChange = (e) => {
     setvalues({ ...values, [e.target.name]: e.target.value });
+    setManualEdit(true); // Set manualEdit to true when the user types
   };
+
   const handleNavigate = () => {
     const searchVal = { state: { values: values, path: "search" } }
     if (searchVal) {
       navigate("/jobs");
     }
   };
+
   const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser does not support speech recognition.</span>;
   }
-
+  useEffect(() => {
+    if (!manualEdit) {
+      setvalues((prevValues) => ({ ...prevValues, title: transcript }));
+    }
+  }, [transcript, manualEdit]);
   const StartListening = () => {
     SpeechRecognition.startListening();
-    console.log('first')
-    resetTranscript()
-    setIsListening(true)
+    resetTranscript();
+    setIsListening(true);
+    setManualEdit(false);
   };
 
   const stopListening = () => {
@@ -64,11 +74,19 @@ const Home = () => {
     setIsListening(false);
   };
 
-  const combineVal = values?.title || transcript
+
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Backspace" && values.title === "") {
+      resetTranscript();
+    }
+  };
+
+  const combineVal = manualEdit ? values.title : transcript;
 
   return (
     <>
-      {JobStatus == "pending" || categoryStatus == "pending" ? (
+      {JobStatus === "pending" || categoryStatus === "pending" ? (
         <Loader />
       ) : (
         <>
@@ -104,34 +122,25 @@ const Home = () => {
                       />
                       <Input
                         placeholder="Job Title"
-                        // value={values?.title}
                         value={combineVal}
                         name="title"
                         onChange={handleChange}
-
+                        onKeyDown={handleKeyDown}
                         endDecorator={
-                          // <MdKeyboardVoice
-                          //   onClick={StartListening}
-                          //   style={{ color: '#fe9703', fontSize: '18px', cursor: "pointer" }}
-                          // />
-
                           isListening ? (
                             <IoStopCircleOutline
-                              onClick={StartListening}
-                              style={{ color: '#fe9703', fontSize: '18px', cursor: "pointer" }}
-                            />
-                          ) : (
-
-                            <MdKeyboardVoice
                               onClick={stopListening}
                               style={{ color: '#fe9703', fontSize: '18px', cursor: "pointer" }}
                             />
+                          ) : (
+                            <MdKeyboardVoice
+                              onClick={StartListening}
+                              style={{ color: '#fe9703', fontSize: '18px', cursor: "pointer" }}
+                            />
                           )
-
                         }
                         sx={HomeStyle.searchInput}
                       />
-
                       <Button
                         sx={HomeStyle.searchBtn}
                         endDecorator={<ArrowForwardIcon />}
@@ -143,7 +152,6 @@ const Home = () => {
                   </Box>
                 </Grid>
                 <Grid item xs={12} md={5} sx={{ margin: "auto" }}>
-                  {" "}
                   <Box mt={20}>
                     <Box
                       component="img"
